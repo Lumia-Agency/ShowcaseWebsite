f8be3874751a6e7ab7cf0ff6ea3e31dce60ac53c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-08-02 13:44:55 +0200

    :heavy_plus_sign: Add and configure eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxt/eslint",
+    "@nuxtjs/google-fonts"
+  ],
   tailwindcss: {
     cssPath: ["assets/css/tailwind.css", { injectPosition: "first" }],
     configPath: "tailwind.config",
@@ -11,6 +16,16 @@ export default defineNuxtConfig({
   },
   shadcn: {
     prefix: "",
-    componentDir: "./components/ui"
-  }
+    componentDir: "./components/ui",
+  },
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
 });
