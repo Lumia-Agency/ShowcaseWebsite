dfb24142408f051cf191379210f86aadd00fa446
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-08-24 00:59:03 +0200

    :rocket: Add navigation pages
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ pages/work.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-[200dvh] grid place-items-center p-16">
+    <span class="block w-full h-full bg-black" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
