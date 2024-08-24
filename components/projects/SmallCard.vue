2f626272a88fdc69bcb2e4f31c653f53ce2ce0b9
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-08-24 13:27:54 +0200

    :rocket: Add project card
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ components/projects/SmallCard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { Card, CardHeader, CardContent, CardFooter } from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import type { Project } from "~/types/project";
+import { cn } from "~/lib/utils";
+import { useLocalePath } from "#imports";
+
+const props = defineProps<{
+  class?: string;
+  project: Project;
+}>();
+
+const localePath = useLocalePath();
+</script>
+
+<template>
+  <Card :class="cn('relative rounded-2xl isolate', props.class)">
+    <NuxtLink
+      :to="localePath(`/projects/${project.id}`)"
+      class="absolute inset-0 z-10"
+    />
+    <CardHeader>
+      <NuxtImg
+        v-if="project.banner"
+        :src="project.banner"
+        class="h-40 w-full rounded-lg object-cover"
+      />
+      <span
+        v-else
+        class="h-40 w-full bg-secondary rounded-lg"
+      />
+    </CardHeader>
+    <CardContent class="flex flex-col gap-6">
+      <div class="flex flex-col">
+        <p class="text-2xl font-bold">
+          {{ project.name }}
+        </p>
+        <span class="text-xs text-muted-foreground">{{ $t(`projectTypes.${project.type}`) }}</span>
+      </div>
+      <p class="leading-relaxed line-clamp-5">
+        {{ project.caption }}
+      </p>
+    </CardContent>
+    <CardFooter>
+      <Button
+        variant="secondary"
+        class="relative z-20"
+      >
+        {{ $t("buttons.seeMore") }}
+      </Button>
+    </CardFooter>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
