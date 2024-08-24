5e79c42c8dd1e7caa7c8c9f58008a0491727966c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-08-24 13:27:34 +0200

    :rocket: Add footer link
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ components/layout/footer/Link.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { useLocalePath } from "#imports";
+import { cn } from "~/lib/utils";
+import { Button } from "~/components/ui/button";
+
+const props = defineProps<{
+  class?: string;
+  to: string;
+}>();
+const localePath = useLocalePath();
+</script>
+
+<template>
+  <Button
+    variant="link"
+    as-child
+  >
+    <NuxtLink
+      :to="localePath(to)"
+      :class="cn('!justify-start', props.class)"
+    >
+      <slot />
+    </NuxtLink>
+  </Button>
+</template>
+
+<style scoped>
+
+</style>
