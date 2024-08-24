4135dd1c77ae78ebbffef767ff817b948fa0ec78
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-08-24 13:27:15 +0200

    :rocket: Add testimonials mocks
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ assets/mocks/testimonials.ts
@@ -0,0 +1,44 @@
+import type { Testimonial } from "~/types/testimonials";
+
+export default [
+  {
+    author: {
+      avatar: "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      fullName: "Océane S.",
+      role: "PDG",
+      company: "Meilleure patisserie de france",
+    },
+    mark: 4,
+    comment: "Ils ont transformé notre vision en une réalité lumineuse. Leur équipe a su capter notre essence et la retranscrire de manière innovante. Merci Lumia pour cette collaboration éclatante !",
+  },
+  {
+    author: {
+      avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      fullName: "Alba C.",
+      role: "CEO",
+      company: "Meltdown",
+    },
+    mark: 3,
+    comment: "De la conception à la mise en œuvre, chaque étape a été un pur bonheur. Lumia a surpassé nos attentes et a fait briller notre marque comme jamais auparavant.",
+  },
+  {
+    author: {
+      avatar: "https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      fullName: "Aymeric G.",
+      role: "CEO",
+      company: "Patisse-moi",
+    },
+    mark: 5,
+    comment: "Je ne savais pas que c’était possible de faire ressortir autant de magie de notre site web. Lumia a apporté une touche unique et captivante à notre présence en ligne. Un grand merci à toute l'équipe !",
+  },
+  {
+    author: {
+      avatar: "https://images.unsplash.com/photo-1636041263374-dff82464f619?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      fullName: "Bryan M.",
+      role: "Barman",
+      company: "Meltdown",
+    },
+    mark: 2,
+    comment: "Nous étions dans le noir avant de rencontrer Lumia. Leur expertise et leur créativité ont illuminé notre projet et nous ont guidés vers un succès retentissant. Absolument recommandé !",
+  },
+] as Testimonial[];
